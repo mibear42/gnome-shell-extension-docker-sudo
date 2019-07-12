1a61093845edd8d694fe31876e0b15f59da66f52
MiBear42 <mibear42@schloss66.de>
2019-07-12 20:04:15 +0200

    Set new extension name

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 # Simple bash script to build the Gnome Shell extension
 
 echo "Zipping the extension..."
-zip -r docker_status@gpouilloux.zip . -x *.git*
+zip -r docker_status@mibear42.zip . -x *.git*
 
 echo "Done building."
